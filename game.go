78f2046aad5418682ee61514146ec4fd7c5aa1b1
Georgy Moshkin <louielouie314@gmail.com>
2017-05-03 22:12:36 +0300

    Make use of termbox's functions

--- game.go
+++ game.go
@@ -6,78 +6,23 @@ import (
     "github.com/nsf/termbox-go"
 )
 
-type Color interface {
-    GetTermColor() int
-}
-
-type TermColor int
-
-func (clr TermColor) GetTermColor() int {
-    return int(clr)
-}
-
-func GetTermFg(clr int) string {
-    if clr > 0 {
-        return fmt.Sprintf("38;5;%d", clr)
-    } else {
-        return "90"
+func Init() {
+    err := termbox.Init()
+    if err != nil {
+        panic(err)
     }
 }
 
-func GetTermBg(clr int) string {
-    if clr > 0 {
-        return fmt.Sprintf("48;5;%d", clr)
-    } else {
-        return "49"
-    }
-}
-
-func ToTerm(clr uint8) int {
-    ratio := 6 / 256.0
-    return int(float64(clr) * ratio)
-}
-
-type RGBColor struct {
-    red uint8
-    green uint8
-    blue uint8
-    alpha uint8
-}
-
-func (clr RGBColor) GetTermColor() int {
-    if clr.alpha < 255 / 2 {
-        return -1
-    } else {
-        r := ToTerm(clr.red)
-        g := ToTerm(clr.green)
-        b := ToTerm(clr.blue)
-        return int(16 + 36 * r + 6 * g + b)
-    }
-}
-
-func Hex2RGB(hex_color string) RGBColor {
-    start := 0
-    if hex_color[0] == '#' {
-        start = 1
-    }
-    vals, _ := hex.DecodeString(hex_color[start:])
-    a := uint8(255)
-    if len(vals) > 3 {
-        a = vals[3]
-    }
-    rgb := RGBColor{ vals[0], vals[1], vals[2], a }
-    return rgb
-}
-
-func DisplayDixel(top Color, bottom Color) {
-    bg := GetTermBg(top.GetTermColor())
-    fg := GetTermFg(bottom.GetTermColor())
-    fmt.Printf("\033[%sm\033[%sm▄\033[0m", bg, fg)
+func Close() {
+    termbox.Close()
 }
 
 func main() {
-    DisplayDixel(TermColor(-1), Hex2RGB("#ff0088"))
-    DisplayDixel(TermColor(-1), TermColor(-1))
-    DisplayDixel(Hex2RGB("#008800"), TermColor(-1))
-    fmt.Println(GetTermSize())
+    Init()
+    cols, rows := termbox.Size()
+    screen := MakeScreen(uint(cols), uint(rows * 2))
+    screen.Display()
+    event := termbox.PollEvent()
+    Close()
+    fmt.Println(event)
 }
