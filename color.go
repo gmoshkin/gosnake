2e819b998a8fb2a471a7a102c723a381d14d2459
Georgy Moshkin <louielouie314@gmail.com>
2017-05-03 22:14:51 +0300

    fixeup

--- color.go
+++ color.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+    "fmt"
+    "encoding/hex"
+)
+
 type Color interface {
     GetTermColor() int
 }
--- game.go
+++ game.go
@@ -2,7 +2,6 @@ package main
 
 import (
     "fmt"
-    "encoding/hex"
     "github.com/nsf/termbox-go"
 )
 
