6c9550447af23ed5684b336f61585aedf2d33550
Georgy Moshkin <louielouie314@gmail.com>
2017-05-04 03:23:09 +0300

    Make it a termloop game

--- game.go
+++ game.go
@@ -1,30 +1,53 @@
 package main
 
 import (
-    "fmt"
-    "github.com/nsf/termbox-go"
+    tl "github.com/JoelOtter/termloop"
 )
 
-func Init() {
-    err := termbox.Init()
-    if err != nil {
-        panic(err)
-    }
-    termbox.SetOutputMode(termbox.Output256)
+type Snake struct {
+    *tl.Entity
 }
 
-func Close() {
-    termbox.Close()
+func NewSnake(x, y int, color tl.Attr) *Snake {
+    s := &Snake{tl.NewEntity(x, y, 1, 1)}
+    s.SetCell(0, 0, &tl.Cell{Fg: color, Ch: 's'})
+    return s
 }
+
+func (s *Snake) Tick(event tl.Event) {
+    charActions := map[rune]([2]int){
+        'l': { 1, 0 }, 'h': { -1, 0 }, 'k': { 0, -1 }, 'j': { 0, 1 },
+    }
+    keyActions := map[tl.Key]([2]int){
+        tl.KeyArrowRight: { 1, 0 },
+        tl.KeyArrowLeft: { -1, 0 },
+        tl.KeyArrowUp: { 0, -1 },
+        tl.KeyArrowDown: { 0, 1 },
+    }
+    if event.Type == tl.EventKey {
+        x, y := s.Position()
+        ofs, ok := keyActions[event.Key]
+        if ok {
+            s.SetPosition(x + ofs[0], y + ofs[1])
+        }
+        ofs, ok = charActions[event.Ch]
+        if ok {
+            s.SetPosition(x + ofs[0], y + ofs[1])
+        }
+    }
+}
+
+var g *tl.Game
 
 func main() {
-    Init()
-    cols, rows := termbox.Size()
-    screen := MakeScreen(cols, rows * 2)
-    screen.SetCell(6, 6, 7)
-    screen.Display()
-    event := termbox.PollEvent()
-    Close()
-    fmt.Println(event)
-    fmt.Println(screen.GetWidth(), screen.GetHeight())
+    g = tl.NewGame()
+    g.Screen().SetFps(60)
+    g.SetDebugOn(true)
+    l := tl.NewBaseLevel(tl.Cell{
+        Bg: tl.Attr(tl.ColorBlue),
+    })
+    l.AddEntity(NewSnake(1, 1, tl.ColorWhite))
+    g.Screen().SetLevel(l)
+    g.Screen().AddEntity(tl.NewFpsText(0, 0, tl.ColorWhite, tl.ColorDefault, 0.5))
+    g.Start()
 }
