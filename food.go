89ae003878d2ef05ff48d8a422abc3fe2184637d
Georgy Moshkin <louielouie314@gmail.com>
2017-08-19 21:44:37 +0300

    Extract food stuff to a new file

---
+++ food.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+    tl "github.com/gmoshkin/termloop"
+    "container/list"
+)
+
+const (
+    FoodColor tl.Attr = tl.ColorMagenta
+    FoodFrequency float64 = 5.0
+)
+
+///////////////////////////////////// Food /////////////////////////////////////
+
+type Food struct {
+    *tl.Entity
+}
+
+func NewFood(x, y int) *Food {
+    f := &Food { tl.NewEntity(x, y, 1, 1) }
+    f.SetCell(0, 0, &tl.Cell { Bg: FoodColor, Ch: 'f' })
+    return f
+}
+
+///////////////////////////////// FoodManager //////////////////////////////////
+
+type FoodManager struct {
+    foods *list.List
+    updatePeriod float64
+    lastUpdate float64
+}
+
+func NewFoodManager() *FoodManager {
+    return &FoodManager { list.New(), FoodFrequency, FoodFrequency - 0.5 }
+}
+
+func (fm *FoodManager) MakeFood(x, y int) *Food {
+    return NewFood(x, y)
+}
+
+func (fm *FoodManager) IsTime(timeDelta float64) bool {
+    fm.lastUpdate += timeDelta
+    if fm.lastUpdate > fm.updatePeriod {
+        fm.lastUpdate -= fm.updatePeriod
+        return true
+    }
+    return false
+}
--- level.go
+++ level.go
@@ -2,7 +2,6 @@ package main
 
 import (
     tl "github.com/gmoshkin/termloop"
-    "container/list"
     "math/rand"
 )
 
@@ -10,8 +9,6 @@ const (
     LevelBorderColor tl.Attr = tl.ColorGreen
     LevelBorderWidth int = 3
     LevelColor tl.Attr = tl.ColorBlack
-    FoodColor tl.Attr = tl.ColorMagenta
-    FoodFrequency float64 = 5.0
     MaxTries int = 100
 )
 
@@ -48,43 +45,6 @@ func NewField() *Field {
         tl.NewRectangle(LevelBorderWidth, LevelBorderWidth, 1, 1, LevelColor),
         LevelBorderWidth,
     }
-}
-
-///////////////////////////////////// Food /////////////////////////////////////
-
-type Food struct {
-    *tl.Entity
-}
-
-func NewFood(x, y int) *Food {
-    f := &Food { tl.NewEntity(x, y, 1, 1) }
-    f.SetCell(0, 0, &tl.Cell { Bg: FoodColor, Ch: 'f' })
-    return f
-}
-
-///////////////////////////////// FoodManager //////////////////////////////////
-
-type FoodManager struct {
-    foods *list.List
-    updatePeriod float64
-    lastUpdate float64
-}
-
-func NewFoodManager() *FoodManager {
-    return &FoodManager { list.New(), FoodFrequency, FoodFrequency - 0.5 }
-}
-
-func (fm *FoodManager) MakeFood(x, y int) *Food {
-    return NewFood(x, y)
-}
-
-func (fm *FoodManager) IsTime(timeDelta float64) bool {
-    fm.lastUpdate += timeDelta
-    if fm.lastUpdate > fm.updatePeriod {
-        fm.lastUpdate -= fm.updatePeriod
-        return true
-    }
-    return false
 }
 
 ////////////////////////////////// SnakeLevel //////////////////////////////////
